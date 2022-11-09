37a6a9f42baa7ee902cba0ae871b201a9ae7cc49
rohanop <rohanopdev@gmail.com>
2022-11-10 02:37:11 +0530

    Added: register api

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,68 +1,91 @@
 import React, { useState } from "react";
 import { Button, ButtonGroup, Form, ToggleButton } from "react-bootstrap";
-import { Formik } from 'formik';
-import * as Yup from 'yup';
+import { Formik } from "formik";
+import * as Yup from "yup";
 import { Link } from "react-router-dom";
 
-const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
 const schema = Yup.object().shape({
   name: Yup.string().required(),
   userId: Yup.string().required(),
-  email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
-  password: Yup.string().max(255).required('Password is required'),
-  mobile: Yup.string().required().matches(phoneRegExp, 'Phone number is not valid'),
-  techname: Yup.string().required(),
+  email: Yup.string()
+    .email("Must be a valid email")
+    .max(255)
+    .required("Email is required"),
+  password: Yup.string().max(255).required("Password is required"),
+  mobile: Yup.string()
+    .required()
+    .matches(phoneRegExp, "Phone number is not valid"),
+  techname: Yup.string().optional(),
 });
 
+const REGISTER_URL = "http://localhost:8081/api/users/register";
 
 export default function Register() {
-
-  const [radioValue, setRadioValue] = useState('1');
+  const [errorMsg, setErrorMsg] = useState("");
+  const [radioValue, setRadioValue] = useState("1");
 
   const radios = [
-    { name: 'Student', value: '1' },
-    { name: 'Trainer', value: '2' },
+    { name: "Student", value: "1" },
+    { name: "Trainer", value: "2" },
   ];
 
   return (
     <div className="w-full h-full flex flex-col items-center">
       <div className=" pt-8 pb-10">
         <h1 className="text-5xl font-bold text-center mb-3">Register</h1>
-        <span className="text-sm font-semibold">Already registered?   <Link className="text-blue-700 " to={'/login'}>Login</Link> </span>
+        <span className="text-sm font-semibold">
+          Already registered?{" "}
+          <Link className="text-blue-700 " to={"/login"}>
+            Login
+          </Link>{" "}
+        </span>
       </div>
       <div className="w-full max-w-md bg-white p-4 border rounded-md">
         <Formik
           validationSchema={schema}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify({
-                ...values,
-                userType: radioValue
-              }, null, 2));
-              setSubmitting(false);
-            }, 400);
+          onSubmit={async (values) => {
+            try {
+              setErrorMsg("");
+              const response = await fetch(REGISTER_URL, {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                redirect: "follow",
+                referrerPolicy: "no-referrer",
+                body: JSON.stringify({
+                  userid: values.userId,
+                  password: values.password,
+                  name: values.name,
+                  email: values.email,
+                  moblie: values.mobile,
+                  usertype: radioValue,
+                  techname: values.techname,
+                }),
+              });
+              console.log(response.json());
+              setAuth(values);
+            } catch (error) {
+              console.log(error);
+              setErrorMsg("Login Failed!");
+            }
           }}
           initialValues={{
-            name: '',
-            email: '',
-            mobile: '',
-            userId: '',
-            password: '',
-            userType: '',
-            techname: 'Tech',
+            name: "",
+            email: "",
+            mobile: "",
+            userId: "",
+            password: "",
+            userType: "",
+            techname: "",
           }}
         >
-          {({
-            handleSubmit,
-            handleChange,
-            handleBlur,
-            values,
-            errors,
-          }) => (
-
+          {({ handleSubmit, handleChange, handleBlur, values, errors }) => (
             <Form className="space-y-2" onSubmit={handleSubmit}>
-
               <ButtonGroup className="w-full mb-2">
                 {radios.map((radio, idx) => (
                   <ToggleButton
@@ -70,7 +93,9 @@ export default function Register() {
                     id={`radio-${idx}`}
                     type="radio"
                     name="userType"
-                    variant={radioValue === radio.value ? 'primary' : 'outline-primary'}
+                    variant={
+                      radioValue === radio.value ? "primary" : "outline-primary"
+                    }
                     value={radio.value}
                     checked={radioValue === radio.value}
                     onChange={(e) => setRadioValue(e.currentTarget.value)}
@@ -110,7 +135,7 @@ export default function Register() {
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group className="" >
+              <Form.Group className="">
                 <Form.Label>Mobile Number</Form.Label>
                 <Form.Control
                   type="tel"
@@ -140,8 +165,7 @@ export default function Register() {
                 </Form.Control.Feedback>
               </Form.Group>
 
-
-              <Form.Group className="" >
+              <Form.Group className="">
                 <Form.Label>Password</Form.Label>
                 <Form.Control
                   type="password"
@@ -156,7 +180,7 @@ export default function Register() {
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group hidden={radioValue != '2'}>
+              <Form.Group hidden={radioValue != "2"}>
                 <Form.Label>Technology name</Form.Label>
                 <Form.Control
                   type="text"
@@ -172,15 +196,23 @@ export default function Register() {
               </Form.Group>
 
               <Form.Group className="mt-3">
-                <Button className="w-full bg-blue-700 hover:opacity-95" type="submit">
+                <Button
+                  className="w-full bg-blue-700 hover:opacity-95"
+                  type="submit"
+                >
                   Submit
                 </Button>
               </Form.Group>
+
+              {errorMsg && (
+                <div>
+                  <span className="text-red-700 text-sm">{errorMsg}</span>
+                </div>
+              )}
             </Form>
           )}
         </Formik>
-
       </div>
-    </div >
+    </div>
   );
 }
